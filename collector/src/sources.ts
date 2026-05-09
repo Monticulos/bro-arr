699ea2ca4af095724818c2b7d51f6cf4d0c6800e
Monticulos <131489541+Monticulos@users.noreply.github.com>
2026-05-09 23:06:18 +0200

    refactor: src folder structure (#3)

--- collector/src/sources.ts
+++ collector/src/sources.ts
@@ -5,11 +5,34 @@ export interface Source {
 }
 
 export const TARGET_SOURCES: Source[] = [
-  { url: "https://www.bronnoy.kommune.no/", name: "Brønnøy kommune", selector: "#bottom-boxes" },
-  { url: "https://www.bronnoy.kommune.no/kino/", name: "Brønnøy kino", selector: ".InnholdTabell" },
-  { url: "https://www.cafekred.no/arrangementer", name: "Kred", selector: ".slides" },
-  { url: "https://cashbar.no/arrangementer", name: "Cash bar", selector: ".entry-content"},
-  { url: "https://bronnoybibliotek.no/arrangementer#/", name: "Brønnøy bibliotek", selector: ".arena-events-container" },
-  { url: "https://www.havnesenteret.no/dette-skjer", name: "Havnesenteret", selector: "main" },
-  { url: "https://www.bronnoy.kirken.no/Kalender", name: "Brønnøy kirke", selector: ".calendar" },
+  {
+    url: "https://www.bronnoy.kommune.no/",
+    name: "Brønnøy kommune",
+    selector: "#bottom-boxes",
+  },
+  {
+    url: "https://www.bronnoy.kommune.no/kino/",
+    name: "Brønnøy kino",
+    selector: ".InnholdTabell",
+  },
+  {
+    url: "https://cashbar.no/arrangementer",
+    name: "Cash bar",
+    selector: ".entry-content",
+  },
+  {
+    url: "https://bronnoybibliotek.no/arrangementer#/",
+    name: "Brønnøy bibliotek",
+    selector: ".arena-events-container",
+  },
+  {
+    url: "https://www.havnesenteret.no/dette-skjer",
+    name: "Havnesenteret",
+    selector: "main",
+  },
+  {
+    url: "https://www.bronnoy.kirken.no/Kalender",
+    name: "Brønnøy kirke",
+    selector: ".calendar",
+  },
 ];
